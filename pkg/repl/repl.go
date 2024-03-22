4ede055bfbbd4e35c589c308f911aa54b83f1018
ArtificialLegacy <redsbro457@gmail.com>
2024-03-22 07:18:08 -0400

    3.5 - evaluating expressions

--- pkg/repl/repl.go
+++ pkg/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/ArtificialLegacy/monkey/pkg/evaluator"
 	"github.com/ArtificialLegacy/monkey/pkg/lexer"
 	"github.com/ArtificialLegacy/monkey/pkg/parser"
 )
@@ -45,8 +46,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
