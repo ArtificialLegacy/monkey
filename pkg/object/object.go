5d947af70b23870db529b7de9d258eb0ac7bd54c
ArtificialLegacy <redsbro457@gmail.com>
2024-03-26 08:45:45 -0400

    4.3 - built in functions

--- pkg/object/object.go
+++ pkg/object/object.go
@@ -18,6 +18,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -121,4 +122,18 @@ func (s *String) Type() ObjectType {
 
 func (s *String) Inspect() string {
 	return s.Value
+}
+
+type BuiltinFunction func(args ...Object) Object
+
+type BuiltIn struct {
+	Fn BuiltinFunction
+}
+
+func (b *BuiltIn) Type() ObjectType {
+	return BUILTIN_OBJ
+}
+
+func (b *BuiltIn) Inspect() string {
+	return "builtin function"
 }
