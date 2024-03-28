0527bd102af34b4fd36ab8fbc9abf6229cb79a14
ArtificialLegacy <redsbro457@gmail.com>
2024-03-28 12:42:10 -0400

    5.5 - macros

--- pkg/lexer/lexer_test.go
+++ pkg/lexer/lexer_test.go
@@ -34,6 +34,8 @@ func TestNextToken(t *testing.T) {
 
 		[1, 2];
 		{"foo": "bar"}
+
+		macro(x, y) { x + y; };
 	`
 
 	tests := []struct {
@@ -135,6 +137,20 @@ func TestNextToken(t *testing.T) {
 		{token.COLON, ":"},
 		{token.STRING, "bar"},
 		{token.RBRACE, "}"},
+
+		{token.MACRO, "macro"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
 	}
