6224dea180b73c19abd192f1fd109c62001d7010
ArtificialLegacy <redsbro457@gmail.com>
2024-04-30 15:43:51 -0400

    compiler - ch6

--- pkg/code/code.go
+++ pkg/code/code.go
@@ -74,6 +74,11 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
+	OpHash
+
+	OpIndex
 )
 
 type Definition struct {
@@ -107,6 +112,11 @@ var definitions = map[Opcode]*Definition{
 
 	OpGetGlobal: {"OpGetGlobal", []int{2}},
 	OpSetGlobal: {"OpSetGlobal", []int{2}},
+
+	OpArray: {"OpArray", []int{2}},
+	OpHash:  {"OpHash", []int{2}},
+
+	OpIndex: {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
